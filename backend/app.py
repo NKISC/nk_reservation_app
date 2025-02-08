e4708af1cc340ea673c67b7672ee5adf74264ad0
EM <energymc@qq.com>
2025-02-08 17:27:58 +0800

    style: Conform with PEP style

--- backend/app.py
+++ backend/app.py
@@ -11,10 +11,12 @@ app = fastapi.FastAPI()
 async def connection():
     return {"status": "ok"}
 
+
 @app.get("/deploy")
 async def deploy():
     await asyncio.create_subprocess_shell("git pull origin main")
     return "deployed"
+
 
 @app.post("/query/classroom")
 async def query_classroom(cond: data_models.ClassroomQuery):
