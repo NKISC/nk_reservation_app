2728bec974abb0fdaa1d9892d7f56c9dea345144
EM <energymc@qq.com>
2025-04-16 14:57:15 +0800

    fix: Corrected SQL statement and response after operation

--- backend/addition.py
+++ backend/addition.py
@@ -1,12 +1,15 @@
 import sqlite3
-from backend import utils
-from typing import *
+
 
 def addition(classroom: str, noon: bool, applicant_id: str, time_stamp: int):
     db = sqlite3.connect('database.db')
     cursor = db.cursor()
-    # cursor.execute("SELECT id FROM record")
-    # id_rows = cursor.fetchall()
-    # max_id = id_rows[-1][0]
-    cursor.execute("INSERT INTO [record] VALUES (:id, :classroom, :noon, :applicant_id, :time_stamp)",
-                   {"id": id+1, "noon": noon, "classroom": classroom, "applicant_id": applicant_id, "time_stamp": time_stamp})
+    with open("recent_id") as id_file:
+        recent_id = int(id_file.read().strip())
+    try:
+        cursor.execute("INSERT INTO [record] VALUES (:id, :classroom, :noon, :applicant_id, :time_stamp)",
+                       {"id": recent_id + 1, "noon": noon, "classroom": classroom,
+                        "applicant_id": applicant_id, "time_stamp": time_stamp})
+    except BaseException as e:
+        return {"success": False, "error": str(e)}
+    return {"success": True}
