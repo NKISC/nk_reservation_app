8288172c6c5aff9c427eea6bea8e82b469d0c449
EM <energymc@qq.com>
2025-04-30 14:29:58 +0800

    fix: Record update failed

--- backend/utils.py
+++ backend/utils.py
@@ -2,13 +2,15 @@ import sqlite3
 import time
 
 
-def update_record(db: sqlite3.Connection):
+def update_record():
     """
     Delete outdated records.
     :return:
     """
+    db = sqlite3.connect("database.db")
     cursor = db.cursor()
     cursor.execute('delete from record where time_stamp < :threshold',
                    {"threshold": time.time() - 48 * 60 * 60})
     db.commit()
+    db.close()
     return {"success": True}
