d05091bc65360f9b246aeaf2511c35bc4fed019b
EM <energymc@qq.com>
2025-05-03 15:07:57 +0800

    refactor: Resource control with context manager

--- backend/addition.py
+++ backend/addition.py
@@ -11,19 +11,16 @@ def addition(classroom: str, noon: bool, applicant_id: str, time_stamp: int):
                        For instance, if the reservation is on Feb. 1, 2025, the time_stamp will be 1738339200).
     :return:
     """
-    db = sqlite3.connect('database.db')
-    cursor = db.cursor()
-    with open("recent_id") as id_file:
-        recent_id = int(id_file.read().strip())
-    with open("recent_id", "w") as id_file:
-        id_file.write(str(recent_id + 1))
-    try:
-        cursor.execute("INSERT INTO [record] VALUES (:id, :classroom, :noon, :applicant_id, :time_stamp)",
-                       {"id": recent_id + 1, "noon": noon, "classroom": classroom,
-                        "applicant_id": applicant_id, "time_stamp": time_stamp})
-        db.commit()
-    except BaseException as e:
-        db.close()
-        return {"success": False, "error": str(e)}
-    db.close()
-    return {"success": True}
+    with sqlite3.connect('database.db') as db:
+        cursor = db.cursor()
+        with open("recent_id") as id_file:
+            recent_id = int(id_file.read().strip())
+        with open("recent_id", "w") as id_file:
+            id_file.write(str(recent_id + 1))
+        try:
+            cursor.execute("INSERT INTO [record] VALUES (:id, :classroom, :noon, :applicant_id, :time_stamp)",
+                           {"id": recent_id + 1, "noon": noon, "classroom": classroom,
+                            "applicant_id": applicant_id, "time_stamp": time_stamp})
+        except BaseException as e:
+            return {"success": False, "error": str(e)}
+        return {"success": True}
--- backend/utils.py
+++ backend/utils.py
@@ -7,10 +7,7 @@ def update_record():
     Delete outdated records.
     :return:
     """
-    db = sqlite3.connect("database.db")
-    cursor = db.cursor()
-    cursor.execute('delete from record where time_stamp < :threshold',
-                   {"threshold": time.time() - 48 * 60 * 60})
-    db.commit()
-    db.close()
-    return {"success": True}
+    with sqlite3.connect("database.db") as db:
+        cursor = db.cursor()
+        cursor.execute('delete from record where time_stamp < :threshold',
+                       {"threshold": time.time() - 48 * 60 * 60})
