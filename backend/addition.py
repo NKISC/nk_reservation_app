213f3b87c6c06d4f00e9fa972d79a0f2830b35f0
maop0 <maopinxian@qq.com>
2025-05-14 16:24:27 +0800

    feat: addition.py indentation

--- backend/addition.py
+++ backend/addition.py
@@ -13,28 +13,28 @@ def add_records(classroom: str, noon: bool, applicant_id: str, time_stamp: int):
     :return:
     """
     with sqlite3.connect('database.db') as db:
-      cursor = db.cursor()
-      with open("recent_id") as id_file:
-          recent_id = int(id_file.read().strip())
-      with open("recent_id", "w") as id_file:
-          id_file.write(str(recent_id + 1))
-      cursor.execute("SELECT permission FROM user_info WHERE id = :applicant_id", {'applicant_id': applicant_id})
-      res = cursor.fetchone()
-      perm = res[0].split(",")
-      del perm[-1]
-      clas = [classroom]
-      no_perm = check_permission(perm, clas)
-      for i in no_perm:
-          if i == classroom:
-              return {"success": False, "error": "no_permission"}
+        cursor = db.cursor()
+        with open("recent_id") as id_file:
+            recent_id = int(id_file.read().strip())
+        with open("recent_id", "w") as id_file:
+            id_file.write(str(recent_id + 1))
+        cursor.execute("SELECT permission FROM user_info WHERE id = :applicant_id", {'applicant_id': applicant_id})
+        res = cursor.fetchone()
+        perm = res[0].split(",")
+        del perm[-1]
+        clas = [classroom]
+        no_perm = check_permission(perm, clas)
+        for i in no_perm:
+            if i == classroom:
+                return {"success": False, "error": "no_permission"}
 
-      try:
-          cursor.execute("INSERT INTO [record] VALUES (:id, :classroom, :noon, :applicant_id, :time_stamp)",
-                         {"id": recent_id + 1, "noon": noon, "classroom": classroom,
-                          "applicant_id": applicant_id, "time_stamp": time_stamp})
-      except BaseException as e:
-          return {"success": False, "error": str(e)}
-      return {"success": True}
+        try:
+            cursor.execute("INSERT INTO [record] VALUES (:id, :classroom, :noon, :applicant_id, :time_stamp)",
+                           {"id": recent_id + 1, "noon": noon, "classroom": classroom,
+                            "applicant_id": applicant_id, "time_stamp": time_stamp})
+        except BaseException as e:
+            return {"success": False, "error": str(e)}
+        return {"success": True}
 
 
 def add_user(display_name: str, permission: str):
@@ -56,4 +56,3 @@ def add_user(display_name: str, permission: str):
             return {"success": False, "error": f"Database error: {e}"}
         except Exception as e:
             return {"success": False, "error": f"Unknown error: {e}"}
-
