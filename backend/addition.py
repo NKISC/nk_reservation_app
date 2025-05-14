fe751de1aaaae09e8446de0490faeff3d4702134
maop0 <maopinxian@qq.com>
2025-05-14 10:59:55 +0800

    feat: addition.py: judge user's permission

--- backend/addition.py
+++ backend/addition.py
@@ -1,4 +1,6 @@
 import sqlite3
+from backend import query
+from backend.query import check_permission
 
 
 def addition(classroom: str, noon: bool, applicant_id: str, time_stamp: int):
@@ -8,6 +10,16 @@ def addition(classroom: str, noon: bool, applicant_id: str, time_stamp: int):
         recent_id = int(id_file.read().strip())
     with open("recent_id", "w") as id_file:
         id_file.write(str(recent_id + 1))
+    cursor.execute("SELECT permission FROM user_info WHERE id = :applicant_id", {'applicant_id': applicant_id})
+    res = cursor.fetchone()
+    perm = res[0].split(",")
+    del perm[-1]
+    clas = [classroom]
+    no_perm = check_permission(perm, clas)
+    for i in no_perm:
+        if i == classroom:
+            return {"success": False, "error": "no_permission"}
+
     try:
         cursor.execute("INSERT INTO [record] VALUES (:id, :classroom, :noon, :applicant_id, :time_stamp)",
                        {"id": recent_id + 1, "noon": noon, "classroom": classroom,
