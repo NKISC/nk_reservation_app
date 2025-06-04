4124915563de09dbf6ef00d5099d49e123ed287b
EM <energymc@qq.com>
2025-06-04 15:01:19 +0800

    feat: Search bar in user management

--- miniprogram/pages_individual/admin_tool/adminTool.vue
+++ miniprogram/pages_individual/admin_tool/adminTool.vue
@@ -9,8 +9,10 @@
           用户管理 <span>{{ expandUser ? "-" : "+" }}</span>
         </view>
         <view v-if="expandUser" style="height: 10rpx"></view>
+        <u-search placeholder="用户ID" v-model="userSearch" :clearabled="true" :show-action="false" v-if="expandUser"/>
         <view v-for="(item, index) in users" :key="index" v-if="expandUser">
-          <view class="itembox" style="height: fit-content; width: inherit; background-color: #3A3A3A; color: white; margin: 3%; padding: 3%; font-size: 15px">
+          <view class="itembox" style="height: fit-content; width: inherit; background-color: #3A3A3A; color: white; margin: 3%; padding: 3%; font-size: 15px"
+          v-if="item.id.includes(userSearch)">
             <view style="font-weight: bold; color: grey; margin: 2%">{{ item.id }}</view>
             <view style="display: flex; justify-content: space-between; margin: 2%">
               <text>显示名</text><input v-model="users[index].display" style="background-color: white; color: black; text-align: right; padding: 1%" />
@@ -65,6 +67,7 @@ export default {
       expandUser: false,
       expandReservation: false,
       users: [],
+      userSearch: "",
       reservations: [],
       classrooms: [],
       buildDate: (d) => `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, '0')}-${String(d.getDate()).padStart(2, '0')}`,
